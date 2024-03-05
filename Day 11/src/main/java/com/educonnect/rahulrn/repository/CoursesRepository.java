5ef9212609fdc12a96f660889fdb4fc29ceee90e
Rahul R N <111170131+luhar15@users.noreply.github.com>
2024-03-05 16:15:03 +0530

    Add files via upload

---
+++ Day 11/src/main/java/com/educonnect/rahulrn/repository/CoursesRepository.java
@@ -0,0 +1,9 @@
+package com.educonnect.rahulrn.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educonnect.rahulrn.model.Courses;
+
+public interface CoursesRepository extends JpaRepository<Courses, Integer>{
+
+}
